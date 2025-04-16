a1319f1365a04d4f3756c5e46f45e91ff310b80c
Kim Myoung Jun <myoungjun55@naver.com>
2025-04-16 16:12:32 +0900

    Chore: [Config] 프로토타입 개발 환경 구축
    프로토타입 개발 환경 구축

---
+++ runtracker-prototype/src/test/java/com/runtracker_prototype/RuntrackerPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runtracker_prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuntrackerPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
