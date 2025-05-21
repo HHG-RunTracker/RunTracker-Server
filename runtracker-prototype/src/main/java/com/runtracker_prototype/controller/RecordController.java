78a79be408844b9981ebfe0f1f864766f3c06e18
kjunh972 <kjunh972@gmail.com>
2025-05-21 20:42:59 +0900

    [Add] Record : ApiResponse로 컨트롤러 구조 수정

--- runtracker-prototype/src/main/java/com/runtracker_prototype/controller/RecordController.java
+++ runtracker-prototype/src/main/java/com/runtracker_prototype/controller/RecordController.java
@@ -1,12 +1,12 @@
 package com.runtracker_prototype.controller;
 
+import com.runtracker_prototype.code.CommonResponseCode;
 import com.runtracker_prototype.dto.RecordDTO;
+import com.runtracker_prototype.response.ApiResponse;
 import com.runtracker_prototype.service.RecordService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,29 +25,21 @@ public class RecordController {
     
     @Operation(summary = "전체 기록 불러오기", description = "전체 기록을 시간순으로 반환")
     @GetMapping
-    public ResponseEntity<List<RecordDTO>> getRecords() {
+    public ApiResponse<List<RecordDTO>> getRecords() {
         try {
-            return ResponseEntity
-                    .status(HttpStatus.OK)
-                    .body(new ArrayList<>());
+            return ApiResponse.ok(new ArrayList<>());
         } catch (Exception e) {
-            return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ArrayList<>());
+            return ApiResponse.error(CommonResponseCode.INTERNAL_SERVER_ERROR);
         }
     }
     
     @Operation(summary = "코스별 기록 불러오기", description = "코스별 기록을 시간순으로 반환")
     @GetMapping("/{courseId}")
-    public ResponseEntity<List<RecordDTO>> getRecordsByCourseId(@PathVariable Long courseId) {
+    public ApiResponse<List<RecordDTO>> getRecordsByCourseId(@PathVariable Long courseId) {
         try {
-            return ResponseEntity
-                    .status(HttpStatus.OK)
-                    .body(new ArrayList<>());
+            return ApiResponse.ok(new ArrayList<>());
         } catch (Exception e) {
-            return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ArrayList<>());
+            return ApiResponse.error(CommonResponseCode.INTERNAL_SERVER_ERROR);
         }
     }
 }
