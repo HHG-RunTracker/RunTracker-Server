1d9c970b06fbb61058a0d9b506a2601a6dcb646b
Juunbro <kjunh972@gmail.com>
2025-09-25 19:43:05 +0900

    Feat: [Firebase] Firebase 설정 (#39)

--- runtracker/src/main/java/com/runtracker/global/fcm/FirebaseConfig.java
+++ runtracker/src/main/java/com/runtracker/global/fcm/FirebaseConfig.java
@@ -32,7 +32,8 @@ public class FirebaseConfig {
                 googleCredentials = GoogleCredentials
                         .fromStream(new ClassPathResource(serviceAccountKeyPath).getInputStream());
             } else {
-                return;
+                googleCredentials = GoogleCredentials
+                        .fromStream(new ClassPathResource("firebase/runtracker-a30bb-firebase-adminsdk-fbsvc-9479026564.json").getInputStream());
             }
 
             FirebaseOptions options = FirebaseOptions.builder()
