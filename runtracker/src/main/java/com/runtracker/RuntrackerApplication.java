7b2b826527aaf971d5bbae9d2fc240fab03be25e
Juunbro <kjunh972@gmail.com>
2025-10-20 20:29:14 +0900

    Fix: [db] 디비 시간대 변경 (#75)

--- runtracker/src/main/java/com/runtracker/RuntrackerApplication.java
+++ runtracker/src/main/java/com/runtracker/RuntrackerApplication.java
@@ -1,12 +1,20 @@
 package com.runtracker;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class RuntrackerApplication {
+
+	@PostConstruct
+	public void strted() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RuntrackerApplication.class, args);
